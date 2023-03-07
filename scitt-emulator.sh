38df5e32725f7061741e8a5268d8beeef78a2efa
Orie Steele <orie@or13.io>
2023-03-07 13:38:19 -0600

    Update scitt-emulator.sh
    Signed-off-by: Orie Steele <orie@or13.io>

--- scitt-emulator.sh
+++ scitt-emulator.sh
@@ -6,7 +6,7 @@ set -e
 
 if [ ! -f "venv/bin/activate" ]; then
     echo "Setting up Python virtual environment."
-    python -m venv "venv"
+    python3 -m venv "venv"
     . ./venv/bin/activate
     pip install -q -e .
 else
