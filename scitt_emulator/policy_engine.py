534650d0069d36662ae794f4addf1d4865d2a12d
John Andersen <johnandersenpdx@gmail.com>
2024-03-21 01:31:33 +0000

    Vendor entrypoint_style_load into policy_engine.py
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- scitt_emulator/policy_engine.py
+++ scitt_emulator/policy_engine.py
@@ -97,6 +97,7 @@ import argparse
 import tempfile
 import textwrap
 import datetime
+import importlib
 import traceback
 import itertools
 import subprocess
@@ -115,6 +116,7 @@ from typing import (
     Any,
     Annotated,
     Self,
+    Iterator,
 )
 
 
@@ -138,7 +140,33 @@ from pydantic import (
 from fastapi.testclient import TestClient
 
 
-from scitt_emulator.plugin_helpers import entrypoint_style_load
+def entrypoint_style_load(
+    *args: str, relative: Optional[Union[str, pathlib.Path]] = None
+) -> Iterator[Any]:
+    """
+    Load objects given the entrypoint formatted path to the object. Roughly how
+    the python stdlib docs say entrypoint loading works.
+    """
+    # Push current directory into front of path so we can run things
+    # relative to where we are in the shell
+    if relative is not None:
+        if relative == True:
+            relative = os.getcwd()
+        # str() in case of Path object
+        sys.path.insert(0, str(relative))
+    try:
+        for entry in args:
+            modname, qualname_separator, qualname = entry.partition(":")
+            obj = importlib.import_module(modname)
+            for attr in qualname.split("."):
+                if hasattr(obj, "__getitem__"):
+                    obj = obj[attr]
+                else:
+                    obj = getattr(obj, attr)
+            yield obj
+    finally:
+        if relative is not None:
+            sys.path.pop(0)
 
 
 class PolicyEngineCompleteExitStatuses(enum.Enum):
